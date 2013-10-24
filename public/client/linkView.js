cf41ea7582bdaf35545c8359ca5202c7e0063f3e
Leighton <leighton.kaina@gmail.com>
2013-10-23 21:53:40 -0700

    Displays the most recent click for each website

--- public/client/linkView.js
+++ public/client/linkView.js
@@ -5,7 +5,8 @@ Shortly.LinkView = Backbone.View.extend({
   template: _.template(' \
       <img src="/redirect_icon.png"/> \
       <div class="info"> \
-        <div class="lastVisit"></div> \
+        <% updated_at = updated_at.slice(0,19).replace("T"," ") %> \
+        <div class="lastVisit"><%= updated_at %></div> \
         <div class="visits"><span class="count"><%= visits %></span>Visits</div> \
         <div class="title"><%= title %></div> \
         <div class="original"><%= url %></div> \
